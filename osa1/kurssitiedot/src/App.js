1a9769c2c32aa6ef8f69e70cd2b801a0244139ea
jkruuth <joonas.k.ruuth@student.jyu.fi>
2021-05-20 14:45:37 +0300

    jatko

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Header = (props) => {
+  console.log(props)
   return (
     <h1>{props.course}</h1>
   )
@@ -34,21 +35,31 @@ const Part = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
-      <Header course={course} />
-      <Content osa1={part1} harjoitus1={exercises1}
-               osa2={part2} harjoitus2={exercises2} 
-               osa3={part3} harjoitus3={exercises3}/>
-      <Total eka={exercises1} toka={exercises2} kolmas={exercises3}/>
+      <Header course={course.name} />
+      <Content osa1={course.parts[0].name} harjoitus1={course.parts[0].exercises}
+               osa2={course.parts[1].name} harjoitus2={course.parts[1].exercises} 
+               osa3={course.parts[2].name} harjoitus3={course.parts[2].exercises}/>
+      <Total eka={course.parts[0].exercises} toka={course.parts[1].exercises} kolmas={course.parts[2].exercises}/>
     </>
   )
 }
---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react'
+
+
+const Statistics = (props) => {
+  if (props.good + props.bad + props.neutral === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        No feedback given
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h1>statistics</h1>
+      <StatisticLine text="good" value={props.good} />
+      <StatisticLine text="neutral" value={props.neutral} />
+      <StatisticLine text="bad" value={props.bad} />
+      <StatisticLine text="all" value={props.good+props.neutral+props.bad} />
+      <StatisticLine text="average" value={((props.bad * -1) + 
+                   (props.good * 1) + 
+                   (props.neutral * 0)) / (props
+                   .bad + props.good + props.neutral)} />
+
+      <StatisticLine text="positive" value={props.good/(props.bad + props.good + props.neutral) } merkki='%'/>
+    </div>
+  )
+}
+
+
+const StatisticLine = (props) => {
+  return(
+  <p>{props.text} {props.value} {props.merkki}</p>
+  )
+}
+
+
+const Button = ({ handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGood} text='good' />
+      <Button handleClick={handleNeutral} text='neutral' />
+      <Button handleClick={handleBad} text='bad' />
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+export default App
