b3790863c3b9ebc5d0644caf25a3d4fb98a8f796
jkruuth <joonas.k.ruuth@student.jyu.fi>
2021-05-27 13:45:39 +0300

    osa2 paketissa

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,15 +2,31 @@ import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Person from './components/Person'
+import Notification from './components/Notification'
 import personService from './services/persons'
 import axios from 'axios'
+
+const Onnistunut = ({ message }) => {
+  return (
+  <Notification message={message} color='green'/>
+  )
+}
+
+
+const Epaonnistunut = ({ message }) => {
+  return (
+  <Notification message={message} color='red'/>
+  )
+}
+
 
 const App = () => {
   const [ persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber]  = useState('')
   const [showFiltered, setShowFiltered] = useState('')
-
+  const [ message, setMessage ] = useState('')
+  const [ errorMessage, setErrorMessage ] = useState('')
 
   useEffect(() => {
     personService
@@ -23,13 +39,12 @@ const App = () => {
 
   const handlePoisto = (event,id) => {    
     event.preventDefault()
+    const poistettu = persons.find(person => person.id === id)
     axios.delete(`http://localhost:3001/persons/${id}`)  
-      .then(res => {  
-        console.log(res);  
-        console.log(res.data);  
-    
-        const loput = persons.filter(person => person.id !== id);  
-        setPersons(loput);  
+      .then(res => {
+        const loput = persons.filter(person => person.id !== id);
+        setMessage(`Deleted ${poistettu.name}`)
+        setPersons(loput);
       })  
     
   }
@@ -62,7 +77,18 @@ const App = () => {
         personService
           .update(found.id, changedPerson)
           .then(returned => {
+            setMessage(`Changed ${changedPerson.name} number`)
             setPersons(persons.map(person => person.number !== found.number ? person : returned))
+            setNewName('')
+            setNewNumber('')
+          })
+          .catch(error => {
+            setErrorMessage(
+              `Information of ${changedPerson.name} has already been removed from server`
+            )
+            setNewName('')
+            setNewNumber('')
+            return
           })
           return
       } else {
@@ -74,6 +100,10 @@ const App = () => {
     personService
       .create(personObject)
       .then(returnedPerson => {
+        setMessage(`Added ${returnedPerson.name}`)
+        setTimeout(() => {
+          setMessage(null)
+        }, 3000)
         setPersons(persons.concat(returnedPerson))
         setNewName('')
         setNewNumber('')
@@ -84,7 +114,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-
+      <Onnistunut message={message} />
+      <Epaonnistunut message={errorMessage} />
       <Filter filter={showFiltered} muutos={event => setShowFiltered(event.target.value)} />
 
       <h3>add a new</h3>
